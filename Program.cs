27022008eac47560a739439f55ffa496dfcbe17f
Clement Fombonne <147847743+ClementFombonne@users.noreply.github.com>
2026-03-27 13:34:23 +0000

    2. NuGet packages

--- Program.cs
+++ Program.cs
@@ -1,25 +1,28 @@
-﻿namespace AA_Serverless_NET;
+﻿using Newtonsoft.Json;
+
+namespace AA_Serverless_NET;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Personne utilisateur = new Personne( "Clément", 22);
+        Personne utilisateur = new Personne 
+        { 
+            Nom = "Clément", 
+            Age = 22
+        };
 
-        Console.WriteLine(utilisateur.Hello(true));
+        string json = JsonConvert.SerializeObject(utilisateur, Formatting.Indented);
+
+        Console.WriteLine(json);
     }
 }
 
 class Personne
 {
-    public string Nom { get; set; }
+    public required string Nom { get; set; }
     public int Age { get; set; }
 
-    public Personne(string nom, int age)
-    {
-        Nom = nom;
-        Age = age;
-    }
     public string Hello(bool isLowercase)
     {
         string message = $"hello {Nom}, you are {Age}";
