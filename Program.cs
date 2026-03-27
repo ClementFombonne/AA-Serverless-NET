52bf45cff283e7f95319e562d4480bfe028ba04b
Clement Fombonne <147847743+ClementFombonne@users.noreply.github.com>
2026-03-27 13:20:43 +0000

    1- Hello World!

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AA_Serverless_NET;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Personne utilisateur = new Personne( "Clément", 22);
+
+        Console.WriteLine(utilisateur.Hello(true));
+    }
+}
+
+class Personne
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+
+    public Personne(string nom, int age)
+    {
+        Nom = nom;
+        Age = age;
+    }
+    public string Hello(bool isLowercase)
+    {
+        string message = $"hello {Nom}, you are {Age}";
+        return isLowercase ? message.ToLower() : message.ToUpper();
+    }
+}
